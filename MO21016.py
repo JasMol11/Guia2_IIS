b09324d4b07d8a7690fa6f8d5d890ec4ac1c2944
JasMol11 <mo21016@ues.edu.sv>
2024-04-21 15:30:41 -0600

    My first commit

---
+++ MO21016.py
@@ -0,0 +1,14 @@
+# Jason Alexander Molina Ortiz ~ MO21016 ~ IIS GT08
+# Calculadora de comisiones de vendedores
+
+print("=========================================")
+print("BIENVENIDO A LA CALCULADORA DE COMISIONES")
+print("=========================================")
+
+nombre_trabajador = input("Ingresa tu nombre: ")
+ventas_totales = float(input("Ingresa las ventas totales: $"))
+comision = round(ventas_totales * 0.13, 2)
+
+print("\n============================================================================")
+print(f"¡Felicidades {nombre_trabajador}! Tu comision por las ventas que realizaste es de ${comision}")
+print("============================================================================")
